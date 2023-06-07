07810a292231c93331c6c468c53d8524969b2efc
Chilipinas <evgeniy.belousov.20@list.ru>
2023-06-07 23:23:54 +0300

    вынес модели в отдельный файл

---
+++ app/models.py
@@ -0,0 +1,45 @@
+from typing import List
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from pydantic import BaseModel
+
+# Создание базовой модели
+Base = declarative_base()
+
+# Модель студента
+class Student(Base):
+    __tablename__ = 'students'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    group_id = Column(Integer, ForeignKey('groups.id'))
+    group = relationship('Group', back_populates='students')
+
+# Модель группы
+class Group(Base):
+    __tablename__ = 'groups'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    students = relationship('Student', back_populates='group')
+
+# Модель для создания студента
+class StudentCreate(BaseModel):
+    name: str
+    group_id: int
+
+# Модель для создания группы
+class GroupCreate(BaseModel):
+    name: str
+
+# Модель для ответа с информацией о студенте
+class StudentResponse(BaseModel):
+    id: int
+    name: str
+    group_id: int
+
+# Модель для ответа с информацией о группе
+class GroupResponse(BaseModel):
+    id: int
+    name: str
+    students: List[StudentResponse]
+
